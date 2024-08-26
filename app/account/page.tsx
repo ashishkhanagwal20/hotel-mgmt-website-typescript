52d3d9815ac856cf1dfa8c48960b4a51fc2d6160
ashishkhanagwal20 <ashishstudy20@gmail.com>
2024-08-26 20:18:45 +0530

    Added Sign in option with google

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,12 +1,16 @@
 // a default export react component using typescript
 
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome Ashish
+      Welcome {session?.user?.name}
     </h2>
   );
 }
